a64f290c743c4e94da2111bcc8061c1f787daa26
Abraham Gomez <abraham.nava10@gmail.com>
2023-12-06 21:38:26 -0600

    Se agrega router, links al menu y la configuración inicial de tailwind-css

---
+++ src/components/molecules/Header/Logo.jsx
@@ -0,0 +1,10 @@
+export default function Logo() {
+  return (
+    <div>
+      <img
+        src="https://banneroftruth.org/us/wp-content/themes/banner3/images/content/logo_and_text_Jun2015.png"
+        alt="logo ecommerce"
+      />
+    </div>
+  );
+}
---
+++ src/components/organisms/MainHeader.jsx
@@ -0,0 +1,11 @@
+import Logo from "../molecules/Header/Logo";
+import MainMenu from "../molecules/Header/MainMenu";
+
+export default function MainHeader() {
+  return (
+    <div>
+      <Logo />
+      <MainMenu />
+    </div>
+  );
+}
